5e1244fed145f65b2b53ce7d8c7cb0c5af6f6651
Mark Timothy Advento <mark@adventodesign.com>
2022-04-10 13:31:41 +0800

    Pyramid Algo

---
+++ 10_pyramid/index.js
@@ -0,0 +1,51 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+//using recursion
+function pyramid(n, rows = 0, level = "", cols = 0) {
+  const levelWidth = n + (n - 1);
+  const midpoint = Math.floor(levelWidth / 2);
+  if (n === rows) {
+    return;
+  }
+
+  if (levelWidth === level.length) {
+    console.log(level);
+    return pyramid(n, rows + 1);
+  }
+
+  const add = midpoint - rows <= cols && midpoint + rows >= cols ? "#" : " ";
+  pyramid(n, rows, level + add, cols + 1);
+}
+
+module.exports = pyramid;
+
+// //iterative solution, use range and math
+// function pyramid(n) {
+//   const levelWidth = n + (n - 1);
+//   const midpoint = Math.floor(levelWidth / 2);
+//   for (let rows = 0; rows < n; rows++) {
+//     let level = "";
+//     for (let cols = 0; cols < levelWidth; cols++) {
+//       if (midpoint - rows <= cols && midpoint + rows >= cols) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
